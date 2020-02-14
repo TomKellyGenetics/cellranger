1676eaf60db3154ce41505166a955526a2386284
TomKellyGenetics <tomkellygenetics@gmail.com>
2020-02-14 16:22:32 +0900

    update deprecated scripts

--- lib/rust/chunk_reads/src/main.rs
+++ lib/rust/chunk_reads/src/main.rs
@@ -249,7 +249,8 @@ impl<W: Write> Write for StreamWrapper<W> {
 impl<W: Write> Drop for StreamWrapper<W> {
     fn drop(&mut self) {
         match self.s.take() {
-            Some(s) => {s.finish().expect("could not write file");}
+            Some(s) => {s.finish();}
+            Err(never) => never
             None => {}
         }
     }
